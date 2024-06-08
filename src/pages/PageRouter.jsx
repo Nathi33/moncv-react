a25b027efe6b605d9ebc74b6f1711c52e37b6282
Nathi33 <nathaliedarnaudat@outlook.com>
2024-06-08 14:47:19 +0200

    add section-ml

--- src/pages/PageRouter.jsx
+++ src/pages/PageRouter.jsx
@@ -7,6 +7,7 @@ import Services from "./Services";
 import Realisations from "./Realisations";
 import Blog from "./Blog";
 import Contact from "./Contact";
+import MentionsLegales from "./MentionsLegales";
 
 const PageRouter = () => {
   return (
@@ -18,6 +19,7 @@ const PageRouter = () => {
         <Route path="/realisations" element={<Realisations />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/mentionslegales" element={<MentionsLegales />} />
       </Route>
     </Routes>
   );
